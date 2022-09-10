6643bff98bdac73439e9c33beb415fc124db64ae
mehulkumar26 <mehul.kumar26@gmail.com>
2022-09-11 00:17:10 +0530

    Update made

--- gitupdate.js
+++ gitupdate.js
@@ -1,5 +1,10 @@
 function git(){
 
-    console.log("Updating the code for git");
-    
+    function print(){
+    var text1 = "Updating the code for git";
+    var text2 = "Update the code for the branch, we are in the new branch";
+
+    }
 }
+
+module.exports = new git();
---
+++ specGit.js
@@ -0,0 +1,12 @@
+describe('PhotoCommerce', function () {
+
+    var obj = require("./gitupdate.js");
+
+    it('git verification', function(){
+
+        console.log(obj.text1);
+        console.log(obj.text2);
+    });
+
+
+});
